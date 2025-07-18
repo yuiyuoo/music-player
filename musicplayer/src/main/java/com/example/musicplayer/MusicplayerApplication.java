7c271bb186a732e2a333764d201872f00a6e6729
yutranhi <vtranh6858@gmail.com>
2025-07-18 23:47:23 +0200

    Add backend start state

---
+++ musicplayer/src/main/java/com/example/musicplayer/MusicplayerApplication.java
@@ -0,0 +1,13 @@
+package com.example.musicplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicplayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicplayerApplication.class, args);
+	}
+
+}
